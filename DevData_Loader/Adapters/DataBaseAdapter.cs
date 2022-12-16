07bc439c0cd6a8e48ca2fef8b7c5cdd9ffa7fb3b
joannakrol <joannakrol942@gmail.com>
2022-12-16 22:10:08 +0100

    wyniesienie logiki łączenia z bazą danych do innej klasy, filtrowanie już pobranych danych

---
+++ DevData_Loader/Adapters/DataBaseAdapter.cs
@@ -0,0 +1,61 @@
+﻿using DevData_Loader.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DevData_Loader.Adapters
+{
+    public class DataBaseAdapter
+    {
+        public string ServerName { get; set; }
+        public string DataBaseName { get; set; }
+
+        public DataBaseAdapter(ConfigurationModel model)
+        {
+            ServerName = model.ServerName;
+            DataBaseName = model.DataBaseName;
+        }
+
+        public bool TestConnectionByUser(string login, string password, out string mess)
+        {
+            bool result;
+
+            SqlConnection con = new SqlConnection($"Data Source={ServerName};Initial Catalog={DataBaseName};User id={login};Password={password}");
+
+            try
+            {
+                con.Open();
+                result = true;
+                mess = $"Nawiązano połączanie z bazą daneych {DataBaseName}";
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                mess = $"Nie udało się połączuć z bazą danych {DataBaseName}. Powód: {ex.Message}";
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return result;
+        }
+
+        public DataTable GetSqlResult(string login, string password, string query)
+        {
+            DataTable dt;
+
+            using (SqlConnection con = new SqlConnection($"Data Source={ServerName};Initial Catalog={DataBaseName};User id={login};Password={password}"))
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.CommandType = CommandType.Text;
+                DataSet dataSet = new DataSet();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dataSet);
+                dt = dataSet.Tables[0];
+            }
+
+            return dt;
+        }
+    }
+}
--- DevData_Loader/MainWindow.xaml.cs
+++ DevData_Loader/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data;
 using System.Windows;
 using DevData_Loader.Adapters;
 
@@ -9,10 +7,12 @@ namespace DevData_Loader
     public partial class MainWindow : Window
     {
         private readonly ConfigurationAdapter configAdapter = new ConfigurationAdapter();
+        private readonly DataBaseAdapter dataBaseAdapter;
         
         public MainWindow()
         {
             configAdapter.GetConfiguration("../../../ConfigurationAdapter.json");
+            dataBaseAdapter = new DataBaseAdapter(configAdapter.Config);
 
             InitializeComponent();
         }
@@ -43,27 +43,15 @@ namespace DevData_Loader
             }
             else
             {
-                string serverName = configAdapter.Config.ServerName;
-                string dataBase = configAdapter.Config.DataBaseName;
-                string login = tbLogin.Text;
-                string password = pwPassword.Password;
-
-                SqlConnection con = new SqlConnection($"Data Source={serverName};Initial Catalog={dataBase};User id={login};Password={password}");
-
-                try
+                if (dataBaseAdapter.TestConnectionByUser(tbLogin.Text, pwPassword.Password, out string mess))
                 {
-                    con.Open();
                     btnLoadData.IsEnabled = true;
-                    MessageBox.Show($"Nawiązano połączanie z bazą daneych {dataBase}", "Nawiązano połączenie", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MessageBox.Show(mess, "Nawiązano połączenie", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
-                catch (Exception ex)
+                else
                 {
                     btnLoadData.IsEnabled = false;
-                    MessageBox.Show($"Nie udało się połączuć z bazą danych {dataBase}. Powód: {ex.Message}", "Nieudane logowanie", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                finally
-                {
-                    con.Close();
+                    MessageBox.Show(mess, "Nieudane logowanie", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
@@ -72,26 +60,13 @@ namespace DevData_Loader
         {
             dgData.DataContext = new DataTable();
 
-            string serverName = configAdapter.Config.ServerName;
-            string dataBase = configAdapter.Config.DataBaseName;
-            string login = tbLogin.Text;
-            string password = pwPassword.Password;
+            string sqlCommand = "select * from DevData.INFORMATION_SCHEMA.COLUMNS";
+            
+            var dt = dataBaseAdapter.GetSqlResult(tbLogin.Text, pwPassword.Password, sqlCommand);
 
-            string sqlCommand = "select * from DevData.INFORMATION_SCHEMA.COLUMNS where DATA_TYPE = 'int'";
+            var intTypeColumns = dt.AsEnumerable().Where(r => r.Field<string>("DATA_TYPE") == "int");
 
-            DataTable dt;
-
-            using (SqlConnection con = new SqlConnection($"Data Source={serverName};Initial Catalog={dataBase};User id={login};Password={password}"))
-            {
-                SqlCommand cmd = new SqlCommand(sqlCommand, con);
-                cmd.CommandType = CommandType.Text;
-                DataSet dataSet = new DataSet();
-                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-                adapter.Fill(dataSet);
-                dt = dataSet.Tables[0];
-            }
-
-            dgData.DataContext = dt.DefaultView;
+            dgData.DataContext = intTypeColumns.CopyToDataTable();
         }
 
         private void TbLogin_Change(object sender, RoutedEventArgs e)
